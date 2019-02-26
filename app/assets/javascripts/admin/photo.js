65207c172381df03d6b28e17107fff7fb3ef8930
Peter Carnesciali <pcarn9@gmail.com>
2019-02-25 21:05:32 -0600

    Starts adding sliders to admin page

---
+++ app/assets/javascripts/admin/photo.js
@@ -0,0 +1,4 @@
+// Set body height to full window size, taking into account mobile navigation bars
+$('.only-location-mapped').on('click', function() {
+	alert('hi!')
+});
---
+++ config/initializers/administrate.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+Administrate::Engine.add_stylesheet('admin/photo')
+Administrate::Engine.add_stylesheet('admin/navigation')
+Administrate::Engine.add_javascript('admin/photo')
