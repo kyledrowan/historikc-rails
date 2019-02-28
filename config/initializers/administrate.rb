f4d5af86a4e5571ea0a100598fbe9785801b7ae8
Peter Carnesciali <pcarn9@gmail.com>
2019-02-27 22:24:03 -0600

    adds checkboxes for filters

--- config/initializers/administrate.rb
+++ config/initializers/administrate.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
 
 Administrate::Engine.add_stylesheet('admin/photo')
-Administrate::Engine.add_stylesheet('admin/navigation')
 Administrate::Engine.add_javascript('admin/photo')
