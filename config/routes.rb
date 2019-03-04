5192a886b6bbf246c844be1ff2bb828d6b96c52c
Peter Carnesciali <pcarn9@gmail.com>
2019-03-03 18:20:56 -0600

    adds logout option

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,10 @@ Rails.application.routes.draw do
   root to: 'locations#index'
   resources :locations, only: %i[index show]
 
-  if Rails.env.development?
-    namespace :admin do
-      root to: 'photos#index'
-      resources :locations, :photos
-    end
+  namespace :admin do
+    root to: 'photos#index'
+    resources :locations, :photos
+    get 'logout' => 'admin/logout#logout'
   end
 
   get 'auth/oauth2/callback' => 'auth0#callback'
