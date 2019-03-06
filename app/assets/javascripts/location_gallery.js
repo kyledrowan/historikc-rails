059dfb9dee3dd4acdb785ab14de5bd25bc5393ed
Peter Carnesciali <pcarn9@gmail.com>
2019-03-05 20:21:10 -0600

    ‘formatting’

--- app/assets/javascripts/location_gallery.js
+++ app/assets/javascripts/location_gallery.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
           url:'https://twitter.com/intent/tweet?text={{text}} via @HistoriKC&url={{url}}'
         }
         // Facebook sharing pending Open Graph tags, see https://developers.facebook.com/docs/sharing/web
-        // {id:'facebook', label:'Share on Facebook', url:'https://www.facebook.com/sharer/sharer.php?u={{url}}'}
+        // { id: 'facebook', label: 'Share on Facebook', url: 'https://www.facebook.com/sharer/sharer.php?u={{url}}' }
       ],
       getTextForShare: function(shareButtonData) {
         return $('[data-pid="' + this['index'] + '"]').data('summary') || '';
